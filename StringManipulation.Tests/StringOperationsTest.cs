39f493d51c27ad0b0ed8e71e2e559e815cfd81eb
danielbatres <danielbatresuser@gmail.com>
2023-11-02 12:59:40 -0600

    Creating a test with xUnit

---
+++ StringManipulation.Tests/StringOperationsTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation.Tests {
+    public class StringOperationsTest {
+        [Fact]
+        public void ConcatenateStrings() {
+            var strOperations = new StringOperations();
+
+            var result = strOperations.ConcatenateStrings("Hello", "Platzi");
+
+            Assert.Equal("Hello Platzi", result);
+        }
+    }
+}
---
+++ StringManipulation.Tests/UnitTest1.cs
@@ -0,0 +1,8 @@
+namespace StringManipulation.Tests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
